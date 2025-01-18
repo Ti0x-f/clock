2c9b78e193be1fa61224402ec3ad75c0e10cb4b6
Antoine Facerias <antoine.facerias@gmail.com>
2025-01-18 18:21:34 +0100

    Finished the increment/decrement part so that the values in the label part are updated.

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,45 @@ class App extends React.Component {
       currentSession: 25,
       currentBreak: 5
     };
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
   }
 
-  
+  decrement(sessionOrBreak) {
+    if (sessionOrBreak === "break") {
+      this.setState((prevState) => ({
+        breakLength: Math.max(1, prevState.breakLength - 1),
+        currentBreak: Math.max(1, prevState.breakLength - 1)
+      }));
+    } else if (sessionOrBreak === "session") {
+      this.setState((prevState) => ({
+        sessionLength: Math.max(1, prevState.sessionLength - 1),
+        currentSession: Math.max(1, prevState.sessionLength - 1)
+      }));
+    }
+  }
+
+  increment(sessionOrBreak) {
+    if (sessionOrBreak === "break") {
+      this.setState((prevState) => ({
+        breakLength: Math.min(60, prevState.breakLength + 1),
+        currentBreak: Math.min(60, prevState.breakLength + 1)
+      }));
+    } else if (sessionOrBreak === "session") {
+      this.setState((prevState) => ({
+        sessionLength: Math.min(60, prevState.sessionLength + 1),
+        currentSession: Math.min(60, prevState.sessionLength + 1)
+      }));
+    }
+  }
 
 
 
   render() {
     return (
       <div className = "session-and-break">
-        <Selector id="session" decrement={true} increment={true} value={this.state.sessionLength}/>
-        <Selector id="break" decrement={true} increment={true} value={this.state.breakLength} />
+        <Selector id="session" decrement={this.decrement} increment={this.increment} value={this.state.sessionLength}/>
+        <Selector id="break" decrement={this.decrement} increment={this.increment} value={this.state.breakLength} />
       </div>
     );
   }
@@ -28,11 +56,11 @@ class App extends React.Component {
 
 const Selector = ({id, decrement, increment, value}) => {
   return (
-    <div id="{id}-label">
+    <div id={id + "-label"}>
       <p>{id.charAt(0).toUpperCase() + id.slice(1)} Length</p>
-      <button id="{id}-increment" onClick={increment}>+</button>
-      <p id="{id}-length">{value}</p>
-      <button id="{id}-decrement" onClick={decrement}>-</button>
+      <button id={id + "-increment"} onClick={() => increment(id)}>+</button>
+      <p id={id + "-length"}>{value}</p>
+      <button id={id + "-decrement"} onClick={() => decrement(id)}>-</button>
     </div>
   );
 
